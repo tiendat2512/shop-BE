6a9af9f694f137f774d12d02d908335009a3a0da
tiendat2512 <datlanh2512@gmail.com>
2023-11-30 22:30:09 +0700

    Upload Ver

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const port = process.PORT || 3001
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.json ())
 app.use(cookieParser())
 
 routes(app);
